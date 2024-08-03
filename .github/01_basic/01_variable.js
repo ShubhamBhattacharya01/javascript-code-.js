6dca26489856253546c1caf310c0620f37dda59b
ShubhamBhattacharya01 <2305101010231@paruluniversity.ac.in>
2024-08-03 12:51:35 +0000

    variable done

---
+++ .github/01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId=14432
+var accountEmail="shubhambhattacharya@gmail.com"
+let accountPassword="12345678"
+accountCity="bihar"
+let accountState;
+
+//accountId=342
+
+accountEmail="shubham@gmail.com"
+accountPassword="34742634212"
+accountCity="gujrat"
+
+console.table([ accountId,accountEmail,accountPassword,accountCity,accountState])
