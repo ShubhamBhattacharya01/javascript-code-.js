d5130d9cfc271c4f6322313a1ffb02b68dd40d60
ShubhamBhattacharya01 <2305101010231@paruluniversity.ac.in>
2024-08-03 13:27:28 +0000

    datatype

---
+++ .github/01_basic/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict;"//treat all js code as newser version
+let name="shubham"
+let age =19
+let state;
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
+
+// alert("welcome")
