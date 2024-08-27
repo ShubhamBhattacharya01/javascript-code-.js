4f9fbbef98944c69a64811d23ed3d4ffc3244faf
ShubhamBhattacharya01 <2305101010231@paruluniversity.ac.in>
2024-08-27 17:25:41 +0000

    conversion opeartioins

---
+++ .github/01_basic/conversion.js
@@ -0,0 +1,11 @@
+let score="33abv";
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueINnumber=Number(score)
+// console.log( valueINnumber)
+
+let isLoggedIn=""
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
