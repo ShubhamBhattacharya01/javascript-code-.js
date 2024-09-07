b66ed94b3159cef0b4dca846f6c9d2a52b2bf39c
ShubhamBhattacharya01 <2305101010231@paruluniversity.ac.in>
2024-09-07 12:05:22 +0000

    new 2

--- .github/01_basic/convert.js
+++ .github/01_basic/convert.js
@@ -1,5 +1,16 @@
-let str1=hello;
-let str2= world;
-let str3=str1+str2;
+// let str1="hello";
+// let str2=" world";
+// let str3=str1+str2;
 
-console.log(str3);
+// console.log(str3);
+
+console.log(2**2);
+
+// to primitive (convert prefer typeof )
+
+console.log("1"+2); // string 
+console.log(1+"2");// string
+//but 
+console.log(1+2+"2");// frist trite like numbers and then string 
+// pehle jo operation use ki gayi hai baki bhi ush trah se trick hoge agar string to sare string ;
+//pehle number then sare number ""(string ko chor ke );
